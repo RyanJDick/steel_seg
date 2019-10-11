3c68bb9c4c3b840df375c41f79157e66f3b9fa4f
Ryan Dick <rjdick95@yahoo.ca>
2019-10-10 22:33:36 -0400

    Migrate pretrained classification notebook to new dataset format with train/val test

--- steel_seg/utils.py
+++ steel_seg/utils.py
@@ -80,3 +80,26 @@ def onehottify(x, n=None, dtype=float):
     x = np.asarray(x)
     n = np.max(x) + 1 if n is None else n
     return np.eye(n, dtype=dtype)[x]
+
+def print_cm(cm, labels, hide_zeroes=False, hide_diagonal=False, hide_threshold=None):
+    """pretty print for confusion matrixes"""
+    columnwidth = max([len(x) for x in labels] + [5])  # 5 is value length
+    empty_cell = " " * columnwidth
+    # Print header
+    print("    " + empty_cell, end=" ")
+    for label in labels:
+        print("%{0}s".format(columnwidth) % label, end=" ")
+    print()
+    # Print rows
+    for i, label1 in enumerate(labels):
+        print("    %{0}s".format(columnwidth) % label1, end=" ")
+        for j in range(len(labels)):
+            cell = "%{0}.2f".format(columnwidth) % cm[i, j]
+            if hide_zeroes:
+                cell = cell if float(cm[i, j]) != 0 else empty_cell
+            if hide_diagonal:
+                cell = cell if i != j else empty_cell
+            if hide_threshold:
+                cell = cell if cm[i, j] > hide_threshold else empty_cell
+            print(cell, end=" ")
+        print()
